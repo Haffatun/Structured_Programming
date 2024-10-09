c95219bb1353fa2e3a3c33fe4658522f466c3c12
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-10-09 17:10:59 +0600

    Update C. Print from N to 1.cpp

--- Recursion/C. Print from N to 1.cpp
+++ Recursion/C. Print from N to 1.cpp
@@ -1,29 +1,28 @@
+//Given a number N.
+//Print all numbers from N to 1 separated by a single space.
+
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define lets_start    int main()
-#define okeyBye       return 0;
-#define ll            long long
-#define p             printf
-#define usi           unsigned int
-#define vi            vector<int>
- 
-void n_to_one(int k)
+
+//Print function
+void PrintNto1(int n)
 {
-    if(k==0)
-        return;
-    if(k==1)
-        cout << k;
-    else
-    {
-        cout << k << " ";
-        n_to_one(k-1);
-    }
+    //Base case
+    if(n==0) return;
+
+    if(n==1) cout << n << endl;
+    else cout << n << " ";
+
+    //Recursive call statement
+    PrintNto1(n-1);
 }
-lets_start
+int main()
 {
-    int n;
-    cin >> n;
-    n_to_one(n);
-    okeyBye
+    //Take an input n
+    int n; cin >> n;
+    //Call the function
+    PrintNto1(n);
+
+    return 0;
 }
+
