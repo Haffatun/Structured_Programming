ff161cebc1b8c788122e56f117b15fe40a12c884
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-10-10 16:06:38 +0600

    Update R. Palindrome Array.cpp

--- Recursion/R. Palindrome Array.cpp
+++ Recursion/R. Palindrome Array.cpp
@@ -1,36 +1,32 @@
+//Given a number N and an array A of N numbers. 
+//Determine if it's palindrome or not.
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define lets_start    int main()
-#define okeyBye       return 0;
-#define ll            long long
-#define p             printf
-#define usi           unsigned int
-#define vi            vector<int>
- 
-int is_pa(int arr1[], int arr2[], int i, int n, int k)
+
+//Check its palindrome or not
+bool checkPalindrome(int arr[], int n, int k)
 {
-    if(n==k)
-        return i;
-    else if(n<k)
-    {
-        if(arr1[n]==arr2[n])
-            i++;
-        is_pa(arr1, arr2, i, n+1, k);
-    }
+    //Base case
+    if(k>=n) return 0;
+
+    //Recursive call
+    checkPalindrome(arr, n-1, k+1);
+
+    if(arr[n]==arr[k]) return true;
+    else return false;
+
 }
-lets_start
+int main()
 {
+    //Take a number
     int n; cin >> n;
-    int arr1[n], arr2[n];
+    //Take an array
+    int arr[n];
     for(int i=0; i<n; ++i)
-    {
-        cin >> arr1[i];
-        arr2[i]=arr1[i];
-    }
- 
-    reverse(arr2, arr2+n);
-    int k= is_pa(arr1, arr2, 0, 0, n);
-    (k==n)? cout << "YES\n": cout << "NO\n";
-    okeyBye
+        cin >> arr[i];
+    //Call the function and print palindrome or not
+    (checkPalindrome(arr, n-1, 0))? cout << "YES\n": cout << "NO\n";
+
+    return 0;
 }
+
