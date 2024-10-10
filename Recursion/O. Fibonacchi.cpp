27575415c875b070cd210bb68c390bb34a5cebcd
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-10-10 15:11:42 +0600

    Update O. Fibonacchi.cpp

--- Recursion/O. Fibonacchi.cpp
+++ Recursion/O. Fibonacchi.cpp
@@ -1,27 +1,27 @@
+//Given a number N.
+//Print the value of the Nth Fibonacci number.
+
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define lets_start    int main()
-#define okeyBye       return 0;
-#define ll            long long
-#define p             printf
-#define usi           unsigned int
-#define vi            vector<int>
- 
-int fib(int n)
+
+//Fibonacci function
+int Fib(int n)
 {
-    if(n==1)
-        return 0;
-    if(n==2)
-        return 1;
-    else
-        return fib(n-1)+fib(n-2);
+    //Base case
+    if(n==1) return 0;
+    if(n==2) return 1;
+
+    //Recursive call
+    return Fib(n-1)+Fib(n-2);
 }
- 
-lets_start
+int main()
 {
+    //Take a number
     int n; cin >> n;
-    cout << fib(n) << endl;
- 
-    okeyBye
+
+    //Call the function and print the return value
+    cout << Fib(n) << endl;
+
+    return 0;
 }
+
